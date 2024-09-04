e709cb6d002c56912bda31cb62187d9a1cf8cbff
ElizabethM2024 <elymolmol@gmail.com>
2024-09-03 19:03:07 -0500

    se completa tarea

--- taller Javascript/taller.js
+++ taller Javascript/taller.js
@@ -27,17 +27,49 @@ Crea un arreglo con tus 3 colores favoritos y muestra el primer color en la cons
 let coloresfavoritos = ["azul", "blanco", "rojo"];
 console.log(coloresfavoritos[0]);
 
-/**
- * 4. Funciones:
+/*  Funciones:
 - Crea una función llamada “suma” que tome dos números como parámetros y retorne su
 suma
 */
 
 function suma(num1, num2) {
     return num1 + num2;
-}
-
+ }
 console.log(suma(5, 3));
+
+/*  JSON:
+Crea un objeto JSON que represente un libro con las propiedades: título, autor y año de 
+publicación.*/
+
+let libro = {
+    titulo: "Despues de ti",
+    autor: "Diana Molina",
+    añoPublicacion: 1981
+ };
+console.log(libro);
+
+/* Desafío final: 
+Escribe un programa que reciba un arreglo de números y retorne un nuevo arreglo con 
+solo los números pares utilizando funciones. */
+
+function filtrarPares(numeros) {
+    return numeros.filter(numero => numero % 2 === 0);
+}
+let arregloNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(filtrarPares(arregloNumeros));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
